c18e00bf800f8addb2208d02c033aed79490cf2c
ArnavSingha <102987879+ArnavSingha@users.noreply.github.com>
2025-12-14 03:25:05 +0530

    solved the image link problem

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -12,52 +12,8 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
+        hostname: '**', // 👈 This single line allows images from ANY website
       },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'm.media-amazon.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'shreemsweetsandbakery.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.news18.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'static.toiimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.istockphoto.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.pixabay.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'instamart-media-assets.swiggy.com',
-      }
     ],
   },
 };
